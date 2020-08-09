afb17a7f55538ae91d08d1eadbca8029bcbdb8a3
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2020-08-09 13:31:43 +0200

    first files

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //My hello world program
+            string HelloWorld = "Hello World", FirstName, LastName;
+            Console.WriteLine(HelloWorld);
+            Console.Write("First Name: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine($"Your full name is {FirstName} {LastName}");
+        }
+    }
+}
